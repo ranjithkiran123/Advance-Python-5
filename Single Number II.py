da8bbd39783be0d2955a82a1dc35d196bc40e96a
ranjithkiran123 <92369725+ranjithkiran123@users.noreply.github.com>
2023-03-06 22:34:28 +0530

    Add files via upload

---
+++ Single Number II.py
@@ -0,0 +1,7 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        numset = set(nums)
+        for num in numset:
+            if bisect.bisect_left(nums, num) + 1 == bisect.bisect_right(nums, num):
+                return num
